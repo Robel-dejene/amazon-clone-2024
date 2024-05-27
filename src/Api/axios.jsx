3cda08784aff2ed5993d9e2a1c03765f164c869c
Robel-dejene <robeldejene33@gmail.com>
2024-05-27 19:16:22 +0300

    second try using axios

--- src/Api/axios.jsx
+++ src/Api/axios.jsx
@@ -1,8 +1,10 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
+	//local instance of firebase functions
 	// baseURL: "http://127.0.0.1:5001/clone-2024-98157/us-central1/api",
-	baseURL: "https://amazon-api-deploy-1-g71o.onrender.com/",
+	//deploy version of amazon server on render.com
+	baseURL: "https://amazon-api-deploy-1-g71o.onrender.com",
 });
 
 export {axiosInstance};
