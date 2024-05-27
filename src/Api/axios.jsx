54a3007c894223408a425bf967aaedde984d1d31
Robel-dejene <robeldejene33@gmail.com>
2024-05-27 19:12:07 +0300

    connecting to back end using axios

--- src/Api/axios.jsx
+++ src/Api/axios.jsx
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-	baseURL: "http://127.0.0.1:5001/clone-2024-98157/us-central1/api",
+	// baseURL: "http://127.0.0.1:5001/clone-2024-98157/us-central1/api",
+	baseURL: "https://amazon-api-deploy-1-g71o.onrender.com/",
 });
 
 export {axiosInstance};
